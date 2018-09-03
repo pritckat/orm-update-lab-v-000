d9b11c4124f4a0f695c08f5a9ea8bfbaacd6cc78
Katrina Pritchard <pritckat@gmail.com>
2018-09-03 18:39:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,9 @@ require_relative "../config/environment.rb"
 
 class Student
 
+  attr_accessor :name, :grade
+  attr_reader :id
+
   def initialize(name, grade, id = nil)
     @name = name
     @grade = grade
