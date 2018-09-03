51196eacc816ef3b86a3b0d172c4a5271aea5b91
Katrina Pritchard <pritckat@gmail.com>
2018-09-03 18:55:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -47,6 +47,18 @@ class Student
     new_student = self.new(row[1], row[2], row[0])
   end
 
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
 
 
 end
